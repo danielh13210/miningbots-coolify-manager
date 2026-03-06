0997e439ba0ded15e327ee854790ffb95ae4bc10
daniel <51289681+danielh13210@users.noreply.github.com>
2026-03-06 12:57:12 +0800

    Add base flask code

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Render index.html from the templates folder
+    return render_template("index.html", instances=['test'])
+
+@app.route("/favicon.ico")
+def favicon():
+    return redirect("/static/favicon.ico")
+
+if __name__ == "__main__":
+    app.run(debug=True)
