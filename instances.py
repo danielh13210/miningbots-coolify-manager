257d03c96809baf935e97564317bd0ba4130b051
daniel <51289681+danielh13210@users.noreply.github.com>
2026-04-01 07:51:41 +0800

    Fix consistency

--- instances.py
+++ instances.py
@@ -83,11 +83,11 @@ def spawn_player(player,instance,instances):
             "Labels": {
                 "player-parent-instance": instance,
                 "traefik.enable": "true",
-                f"traefik.http.routers.{player}-{instance}-mb.rule": f'Host("{player}-{instance}-mb.{os.environ['BASE_DOMAIN']}")',
-                f"traefik.http.routers.{player}-{instance}-mb.entrypoints": "https",
-                f"traefik.http.routers.{player}-{instance}-mb.tls": "true",
-                f"traefik.http.routers.{player}-{instance}-mb.tls.certresolver": "letsencrypt",
-                f"traefik.http.services.{player}-{instance}.loadbalancer.server.port": "9003",
+                f"traefik.http.routers.{instance}-{player}-mb.rule": f'Host("{player}-{instance}-mb.{os.environ['BASE_DOMAIN']}")',
+                f"traefik.http.routers.{instance}-{player}-mb.entrypoints": "https",
+                f"traefik.http.routers.{instance}-{player}-mb.tls": "true",
+                f"traefik.http.routers.{instance}-{player}-mb.tls.certresolver": "letsencrypt",
+                f"traefik.http.services.{instance}-{player}.loadbalancer.server.port": "9003",
                 "observer_key": instances[instance]['observer_key']
             },
             "HostConfig": {
